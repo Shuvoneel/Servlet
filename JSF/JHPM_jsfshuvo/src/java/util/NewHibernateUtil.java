c5860831d93ec5a1cb6883fa504afc19547808a6
Shuvoneel <mhshuvo29@gmail.com>
2019-04-24 12:11:17 +0600

    highlighted

--- JSF/JHPM_jsfshuvo/src/java/util/NewHibernateUtil.java
+++ JSF/JHPM_jsfshuvo/src/java/util/NewHibernateUtil.java
@@ -1,19 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package util;
 
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.SessionFactory;
 
-/**
- * Hibernate Utility class with a convenient method to get Session Factory
- * object.
- *
- * @author User
- */
 public class NewHibernateUtil {
 
     private static final SessionFactory sessionFactory;
